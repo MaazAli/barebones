442d7242c94a55d9763b67eeb665de9dae5eb2c8
MaazAli <maaz_ali@outlook.com>
2014-04-24 22:08:05 -0700

    The home page has a new look. Profile posts are now the next order of business

--- client/js/global_helpers.js
+++ client/js/global_helpers.js
@@ -10,3 +10,7 @@ UI.registerHelper('user_slugged', function(username) {
 	var username_slug = username_slug.replace('_', '-');	
 	return username_slug;
 });
+
+UI.registerHelper('votes_positive', function(votes) {
+	return votes>=0;
+});
--- client/js/profile.js
+++ client/js/profile.js
@@ -1,3 +1,11 @@
 Template.profile_main.recent_questions = function () {
-	return Questions.find({user_id: this._id}, {sort: {created_timestamp: 1}})
+	return Questions.find({user_id: this._id}, {sort: {created_timestamp: -1}})
+}
+
+Template.profile_post_submission_form.profile_post_check = function(username) {
+	if (username == Meteor.user().username) {
+		return "What's on your mind?";
+	} else {
+		return "Write something...";
+	}
 }
